8afedc53280e59732444649cf71b26e55ca5e955
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-05-22 15:44:23 +0530

    feat(register): Add backend error message component
    This commit adds the `BackendErrorMessagesComponent` to the `RegisterComponent`.
    The changes include:
    
    - Importing the `BackendErrorMessagesComponent` in the `RegisterComponent`
    - Adding the `BackendErrorMessagesComponent` to the `imports` array in the
      `RegisterComponent`
    - Adding the `backendErrors` input property to the
      `BackendErrorMessagesComponent`
    
    These changes allow the `RegisterComponent` to display any backend error
    messages that may occur during the registration process.

--- src/app/shared/components/backend-error-messages/backend-error-messages.component.ts
+++ src/app/shared/components/backend-error-messages/backend-error-messages.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { BackendErrorsInterface } from '../../types/backendError.interface';
 
 @Component({
   selector: 'app-backend-error-messages',
   standalone: true,
   imports: [],
   templateUrl: './backend-error-messages.component.html',
-  styleUrl: './backend-error-messages.component.scss'
+  styleUrl: './backend-error-messages.component.scss',
 })
 export class BackendErrorMessagesComponent {
-
+  backendErrors = input.required<BackendErrorsInterface>();
 }
