037450277f17d164e0c966de9a308c721c56cc27
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-05-27 13:38:22 +0530

    feat: Update app scripts and log response in register component
    - Update the `start` script to `dev` in the `package.json` file to better
      reflect the purpose of the script.
    - Update the `subscribe` callback in the `register` method of the
      `RegisterComponent` to log the response as a JSON string instead of a
      simple string. This will provide more detailed information about the
      registration response.

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -1,18 +1,25 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
+import { PersistanceService } from '../../shared/services/persistance.service';
 import { AuthService } from '../services/auth.service';
 import { CurrentUserInterface } from './../../shared/types/currentUser.interface';
 import { authActions } from './actions';
 
 export const registerEffect = createEffect(
-  (actions$ = inject(Actions), authService = inject(AuthService)) => {
+  (
+    actions$ = inject(Actions),
+    authService = inject(AuthService),
+    persistanceService = inject(PersistanceService)
+  ) => {
     return actions$.pipe(
       ofType(authActions.register),
       switchMap(({ request }) => {
         return authService.register(request).pipe(
           map((currentUser: CurrentUserInterface) => {
+            persistanceService.set('accessToken', currentUser.token);
             return authActions.registerSuccess({ currentUser });
           }),
           catchError((errorResponse: HttpErrorResponse) => {
@@ -27,4 +34,19 @@ export const registerEffect = createEffect(
     );
   },
   { functional: true }
+);
+
+export const redirectAfterRegisterEffect = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) => {
+    return actions$.pipe(
+      ofType(authActions.registerSuccess),
+      tap(() => {
+        router.navigateByUrl('/');
+      })
+    );
+  },
+  {
+    functional: true,
+    dispatch: false,
+  }
 );
