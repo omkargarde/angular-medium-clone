00c62e02aa93f812ae44135bad866c3c096cadc4
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-05-28 22:03:05 +0530

    feat(auth): Add actions for registration, login, and fetching current user
    This commit introduces new actions for the authentication module in the
    application. The changes include:
    
    1. Adding actions for user registration, including registering a user,
       successful registration, and registration failure.
    2. Adding actions for user login, including logging in a user, successful
       login, and login failure.
    3. Adding actions for fetching the current user, including fetching the
       current user, successful fetch, and fetch failure.
    
    These changes are necessary to implement the authentication functionality
    in the application, allowing users to register, log in, and fetch their
    current user information.

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,12 +1,19 @@
-import { createActionGroup, props } from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
 import { CurrentUserInterface } from '../../shared/types/currentUser.interface';
 import { LoginRequestInterface } from '../types/loginRequest.interface';
 import { RegisterRequestInterface } from '../types/registerRequest.interface';
 import { BackendErrorsInterface } from './../../shared/types/backendError.interface';
 
 export const authActions = createActionGroup({
+  // This specifies the name of the action group. Here, it's set to "auth" indicating these actions are for authentication.
   source: 'auth',
+  //  This is an object that defines individual actions within the group.
   events: {
+    // <ActionName>: props<{ property1: Type1, property2: Type2 }>(),
+
+    // <ActionName>: This is the name you give to the action (e.g., Register).
+    // props<{ ... }>: This is an optional property that defines the data the action carries. It's an object with properties and their expected data types.
+
     Register: props<{ request: RegisterRequestInterface }>(),
     'Register success': props<{ currentUser: CurrentUserInterface }>(),
     'Register failure': props<{ errors: BackendErrorsInterface }>(),
@@ -14,5 +21,9 @@ export const authActions = createActionGroup({
     Login: props<{ request: LoginRequestInterface }>(),
     'Login success': props<{ currentUser: CurrentUserInterface }>(),
     'Login failure': props<{ errors: BackendErrorsInterface }>(),
+
+    'Get current user': emptyProps(),
+    'Get current user success': props<{ currentUser: CurrentUserInterface }>(),
+    'Get current user failure': emptyProps(),
   },
 });
