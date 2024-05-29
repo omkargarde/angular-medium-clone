7bb50afccdd61e43ee77e49f1439b40d233be41f
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-05-29 20:25:19 +0530

    feat: Add global feed page and feed component
    This commit introduces the following changes:
    
    1. Create the `GlobalFeedComponent` and its corresponding HTML template.
       This component represents the global feed page and includes the `FeedComponent`.
    
    2. Implement the `FeedComponent` and its HTML template. This component
       displays a simple "feed works!" message.
    
    3. Add the necessary routing configuration for the global feed page in the
       `global-feed.routes.ts` file.
    
    4. Implement the `authInterceptor` in the `authInterceptor.ts` file. This
       interceptor adds the authentication token to the HTTP requests if it
       is available in the `PersistanceService`.
    
    These changes lay the foundation for the global feed functionality in the
    application.

---
+++ src/app/global-feed/global-feed/global-feed.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FeedComponent } from '../../shared/components/feed/feed.component';
+
+@Component({
+  selector: 'app-global-feed',
+  standalone: true,
+  imports: [FeedComponent],
+  templateUrl: './global-feed.component.html',
+})
+export class GlobalFeedComponent {}
