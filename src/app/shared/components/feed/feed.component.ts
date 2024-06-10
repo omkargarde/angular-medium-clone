728fda59509129a5037c65850af3d94d2e8e90be
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-06-10 19:54:20 +0530

    pagination update

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -2,19 +2,26 @@ import { AsyncPipe } from '@angular/common';
 import { Component, OnInit, inject, input, signal } from '@angular/core';
 import { ActivatedRoute, Params, Router, RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
+import queryString from 'query-string';
 import { combineLatest } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { ErrorMessageComponent } from '../error-message/error-message.component';
 import { LoadingComponent } from '../loading/loading.component';
+import { PaginationComponent } from '../pagination/pagination.component';
 import { feedActions } from './store/actions';
 import { selectError, selectFeedData, selectIsLoading } from './store/reducers';
-import { PaginationComponent } from "../pagination/pagination.component";
 
 @Component({
-    selector: 'app-feed',
-    standalone: true,
-    templateUrl: './feed.component.html',
-    imports: [AsyncPipe, RouterLink, ErrorMessageComponent, LoadingComponent, PaginationComponent]
+  selector: 'app-feed',
+  standalone: true,
+  templateUrl: './feed.component.html',
+  imports: [
+    AsyncPipe,
+    RouterLink,
+    ErrorMessageComponent,
+    LoadingComponent,
+    PaginationComponent,
+  ],
 })
 export class FeedComponent implements OnInit {
   store = inject(Store);
@@ -34,9 +41,22 @@ export class FeedComponent implements OnInit {
   });
 
   ngOnInit(): void {
-    this.store.dispatch(feedActions.getFeed({ url: this.apiUrl() }));
     this.route.queryParams.subscribe((params: Params) => {
+      console.log('params', params);
       this.currentPage.set(Number(params['page'] || 1));
+      this.fetchFeed();
     });
+  }
+
+  fetchFeed(): void {
+    const offset = this.currentPage() * this.limit() - this.limit();
+    const parsedUrl = queryString.parseUrl(this.apiUrl());
+    const stringifiedParams = queryString.stringify({
+      limit: this.limit(),
+      offset,
+      ...parsedUrl.query,
+    });
+    const apiUrlWithParams = `${parsedUrl.url}?${stringifiedParams}`;
+    this.store.dispatch(feedActions.getFeed({ url: apiUrlWithParams }));
   }
 }
--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,14 +1,31 @@
-import { Component, input } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit, inject, input, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { UtilsService } from '../../services/utils.service';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [],
+  imports: [NgClass, RouterLink],
   templateUrl: './pagination.component.html',
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnInit {
+  utilsService = inject(UtilsService);
+
   total = input(0);
   limit = input(20);
   currentPage = input(1);
   url = input('');
+  pagesCount = signal(1);
+  pages = signal<number[]>([]);
+
+  ngOnInit(): void {
+    this.pagesCount.set(Math.ceil(this.total() / this.limit()));
+    this.pages.set(
+      this.pagesCount() > 0
+        ? this.utilsService.range(1, this.pagesCount())
+        : [],
+    );
+    console.log(this.pages());
+  }
 }
